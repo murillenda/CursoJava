a8c663fe97e39899fafdbc43f2d5df02852f91e8
Murillenda1 <murillo.oliveira@memora.com.br>
2022-06-22 17:02:29 -0300

    Aprendendo a usar JOptionPane

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,7 +1,20 @@
 package cursojava.executavel;
 
+import javax.swing.*;
+
 public class PrimeiraClasseJava {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro programa em Java");
+        String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+        String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
+
+        double carroNumero = Double.parseDouble(carros);
+        double pessoaNumero = Double.parseDouble(pessoas);
+
+        int divisao = (int) (carroNumero / pessoaNumero);
+
+        double resto = carroNumero % pessoaNumero;
+
+        JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao +
+                " carros e sobrou " + resto + " carros");
     }
 }
