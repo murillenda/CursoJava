a54132f90dfa1a6bbb303fa39f7119e4384a4520
Murillenda1 <murillo.oliveira@memora.com.br>
2022-06-23 10:35:10 -0300

    Implementacao de Setter e Getter

--- src/POO/Aluno.java
+++ src/POO/Aluno.java
@@ -2,27 +2,113 @@ package POO;
 
 public class Aluno {
 
-    String nome;
-    int idade;
-    String dataDeNascimento;
-    String registroGeral;
-    String numeroCpf;
-    String nomeMae;
-    String nomePai;
-    String dataMatricula;
-    String nomeEscola;
-    String serieMatriculado;
+    private String nome;
+    private int idade;
+    private String dataDeNascimento;
+    private String registroGeral;
+    private String numeroCpf;
+    private String nomeMae;
+    private String nomePai;
+    private String dataMatricula;
+    private String nomeEscola;
+    private String serieMatriculado;
 
     public Aluno() {
 
     }
-
+    // Setar padrão ao construtor
     public Aluno(String nomePadrao) {
         this.nome = nomePadrao;
     }
-
+    // Setar padrão
     public Aluno(String nomePadrao, int idadePadrao) {
         this.nome = nomePadrao;
         this.idade = idadePadrao;
+    }
+
+    /* Métodos Setters e Getters do Objeto */
+
+    //Set recebe dados
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //Get devolve dados
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getSerieMatriculado() {
+        return serieMatriculado;
+    }
+
+    public void setSerieMatriculado(String serieMatriculado) {
+        this.serieMatriculado = serieMatriculado;
     }
 }
--- src/cursojava/aprendendoconstrutor/AlunoContruir.java
+++ src/cursojava/aprendendoconstrutor/AlunoContruir.java
@@ -3,17 +3,32 @@ package cursojava.aprendendoconstrutor;
 import POO.Aluno;
 
 public class AlunoContruir {
+    public static void main(String[] args) {
+        // new Aluno() é uma instância (Criação de Objeto)(Construtores)
+        // Aluno é um Objeto
+        // aluno1 é uma referência ao objeto aluno
+        Aluno aluno1 = new Aluno(); // João
+        aluno1.setNome("João");
+        aluno1.setIdade(20);
+        aluno1.setDataDeNascimento("28/05/2000");
+        aluno1.setRegistroGeral("213321.32");
+        aluno1.setNumeroCpf("213.321.321.44");
+        aluno1.setNomeMae("Giona");
+        aluno1.setNomePai("Stuart");
+        aluno1.setDataMatricula("21/01/2017");
+        aluno1.setNomeEscola("Juuuninhosgalei");
+        aluno1.setSerieMatriculado("5° ano");
 
-    // new Aluno() é uma instância (Criação de Objeto)(Construtores)
-    // Aluno é um Objeto
-    // aluno1 é uma referência ao objeto aluno
-    Aluno aluno1 = new Aluno(); // João
+        System.out.println("Nome é: " + aluno1.getNome());
+        System.out.println("Idade é " + aluno1.getIdade());
+        System.out.println("Nascimento é " + aluno1.getDataDeNascimento());
 
-    Aluno aluno2 = new Aluno(); // Pedro
+        Aluno aluno2 = new Aluno(); // Pedro
 
-    Aluno aluno3 = new Aluno(); // Alex
+        Aluno aluno3 = new Aluno(); // Alex
 
-    Aluno aluno4 = new Aluno("Matheus");
+        Aluno aluno4 = new Aluno("Matheus");
 
-    Aluno aluno5 = new Aluno("João", 30);
+        Aluno aluno5 = new Aluno("João", 30);
+    }
 }
